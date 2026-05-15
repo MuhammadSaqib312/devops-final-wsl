a18aaf6bd09ebe4bd478d007292820c846154b18
Muhammadsaqib312 <ms0326012@gmail.com>
2026-05-15 09:37:38 +0500

    Initial Laravel DevOps Project Setup

---
+++ resources/views/devops.blade.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>DevOps Final Lab</title>
+</head>
+<body>
+    <h1>Welcome to My DevOps Final Lab Task!</h1>
+    <p>Laravel App is Running Successfully 🚀</p>
+    <p>Tools Used: Git, Docker, AWS, Jenkins, K8s, Terraform, Grafana</p>
+</body>
+</html>
